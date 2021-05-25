06f3e8b273f56d4d5cd32b4edd3a494130b1f2d3
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-05-24 22:08:15 -0500

    Add new states

--- src/model/state.rb
+++ src/model/state.rb
@@ -1,4 +1,11 @@
 module Model
+  module Direction
+    UP = :up
+    RIGHT = :right
+    DOWN = :down
+    LEFT = :left
+  end
+
   class Coord < Struct.new(:row, :col)
   end
 
@@ -12,7 +19,8 @@ module Model
   class Grid < Struct.new(:rows, :cols)
   end
 
-  class State < Struct.new(:snake, :food, :grid)
+  class State < Struct.new(:snake, :food, :grid, :next_direction,
+                           :game_finished)
   end
 
   def self.initial_state
@@ -22,7 +30,9 @@ module Model
         Model::Coord.new(0, 1)
       ]),
       Model::Food.new(4, 4),
-      Model::Grid.new(8, 12)
+      Model::Grid.new(8, 12),
+      Direction::DOWN,
+      false
     )
   end
 end
