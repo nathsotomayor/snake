d43d2212b66fa9d712ea04a248a15453a1ca7c9d
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-05-24 22:53:38 -0500

    Fix some errors Change naming methods in Actions module by class methods and add actions call by Direction submodule from Model module

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -12,25 +12,25 @@ module Actions
 
   private
 
-  def calc_next_position
+  def self.calc_next_position
     current_position = state.snake.positions.first
     case state.next_direction
-    when UP
+    when Model::Direction::UP
       # decrement row
       return Model::Coord.new(current_position.row - 1, current_position.col)
-    when RIGHT
+    when Model::Direction::RIGHT
       # increment col
       return Model::Coord.new(current_position.row, current_position.col + 1)
-    when DOWN
+    when Model::Direction::DOWN
       # increment row
       return Model::Coord.new(current_position.row + 1, current_position.col)
-    when LEFT
+    when Model::Direction::LEFT
       # decrement col
       return Model::Coord.new(current_position.row, current_position.col - 1)
     end
   end
 
-  def position_is_valid?(state, next_position)
+  def self.position_is_valid?(state, next_position)
     # verify that it is in grid
     is_invalid = ((position.row >= state.grid.rows || position.row < 0) ||
                   (position.col >= state.grid.cols || position.col < 0))
@@ -39,13 +39,13 @@ module Actions
     return !(state.snake.positions.include? position)
   end
 
-  def move_snake_to(state, next_position)
+  def self.move_snake_to(state, next_position)
     new_positions = [next_position] + state.snake.positions[0...-1]
     state.snake.positions = new_positions
     state
   end
 
-  def end_game(state)
+  def self.end_game(state)
     state.game_finished = true
     state
   end
